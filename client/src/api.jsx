65a94a680d7768a561d4587c257c5696ca704adc
Wriloant <rana19852006@gmail.com>
2025-05-25 20:55:35 +0600

    2nd Commit

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,12 +1,12 @@
 
 export async function fetchLocations() {
-  const res = await fetch('http://127.0.0.1:5000/get_address_names');
+  const res = await fetch('/api/get_address_names');
   const data = await res.json();
   return data.address;
 }
 
 export async function predictPrice({ address, beds, bath, area }) {
-  const res = await fetch("http://127.0.0.1:5000/predict_rent", {
+  const res = await fetch("/api/predict_rent", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
